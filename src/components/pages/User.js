00a01996741c47b4f17699a470c795b5cc30ebe2
danrey321 <dreyes9@ucmerced.edu>
2020-12-11 07:30:35 -0800

    cors

--- src/components/pages/User.js
+++ src/components/pages/User.js
@@ -1,8 +1,8 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Axios from 'axios';
-import {Redirect} from 'react-router-dom';
+//import {Redirect} from 'react-router-dom';
 
-export default class User extends React.Component{
+export default class User extends Component{
     
     constructor(props){
         super(props);
@@ -34,11 +34,11 @@ export default class User extends React.Component{
     }
     */
 
-    loging = (event) =>{
-        const user = document.getElementById('user').value;
-        const code = document.getElementById('password').value;
+    loging (event){
+        //const user = document.getElementById('user').value;
+        //const code = document.getElementById('password').value;
         event.preventDefault();
-        const req = Axios.post(`http://18.191.201.76:8080/MusicianServer/user?username=${user}&userpass=${code}`, {}, {withCredentials: true})
+        const req = Axios.post('http://18.191.201.76:8080/MusicianServer/user', {}, {withCredentials: true})
             .then((response) =>{
                 console.log(response);
             });
@@ -65,11 +65,11 @@ export default class User extends React.Component{
                 <form onSubmit={this.loging}>
                     <div className="form-group">
                         <label htmlFor="musician-name">UserName</label>
-                        <input type="text" id="user" name="name" value={this.state.username} onChange={this.values} />
+                        <input type="text" id="user" name="name" value={this.state.name} onChange={this.values} />
                     </div>
                     <div className="form-group">
                         <label htmlFor="musician-code">Password</label>
-                        <input type="text" id="password" name="musician-code" value={this.state.password} onChange={this.values} /> 
+                        <input type="text" id="password" placeholder= "password" name="musician-code" value={this.state.code} onChange={this.values} /> 
                     </div>
                     
                     <button id="musician-submit" className="btn">Submit User</button>
